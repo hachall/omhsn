440656154985e9ff3649e8e4531c84254f7912e5
Liliane Momeni <liliane.momeni@btinternet.com>
2017-12-22 18:56:49 +0000

    did events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,54 @@
+class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  def index
+    @events = Event.where.not(latitude: nil, longitude: nil)
+    @markers = @events.map do |resource|
+      {
+        lat: event.latitude,
+        lng: event.longitude
+      }
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @event_coordinates = [{ lat: @event.latitude, lng: @event.longitude }]
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :description, :photo, :photo_cache, :address)
+  end
+
+end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,10 +1,11 @@
 class Resource < ApplicationRecord
   has_many :saved_resources
 
+
   #after_validation :taglines_into_array
   geocoded_by :address
   mount_uploader :photo, PhotoUploader
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode
   #def taglines_into_array
    # self.taglines = self.taglines_as_string.split(' ')
   #end
