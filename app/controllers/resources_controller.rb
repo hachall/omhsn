7d960861dcc8d7ef4267b035811a5319f74ad149
Hamish Hall <hachall@hotmail.com>
2017-12-22 15:09:51 +0000

    edit and update controller and forms for resources

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,5 @@
 class ResourcesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create, :edit, :update]
 
   def index
     @resources = Resource.all
@@ -21,6 +21,16 @@ class ResourcesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @resource = Resource.find(params[:id])
+  end
+
+  def update
+    @resource = Resource.find(params[:id])
+    @resource.update(resource_params)
+    redirect_to resource_path(@resource)
   end
 
 
