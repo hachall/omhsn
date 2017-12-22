ba919520380d813c79a90cddccea2224b353c7fc
Liliane Momeni <liliane.momeni@btinternet.com>
2017-12-22 12:13:17 +0000

    finally

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,24 +1,11 @@
 class Resource < ApplicationRecord
   has_many :saved_resources
 
-  after_validation :taglines_into_array
-  after_validation :contacts_into_array
-
-
+  #after_validation :taglines_into_array
 
   mount_uploader :photo, PhotoUploader
 
-  geocoded_by :address
-  after_validation :geocode
-
-  def taglines_into_array
-    if self.taglines_as_string
-      self.taglines = self.taglines_as_string.split(' ')
-    end
-  end
-  def contacts_into_array
-    if self.contacts_as_string
-      self.contacts = self.contacts_as_string.split(';')
-    end
-  end
+  #def taglines_into_array
+   # self.taglines = self.taglines_as_string.split(' ')
+  #end
 end
