51c9309ab86bf5b98fe3c9a2121b4d37353b0b96
Hamish Hall <hachall@hotmail.com>
2018-01-25 17:35:48 +0000

    autocomplete added for events

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -13,3 +13,16 @@ console.log('hello from autocomplete pack');
     }
   });
 
+  document.addEventListener("DOMContentLoaded", function() {
+    var resourceAddress = document.getElementById('event_address');
+
+    if (resourceAddress) {
+      var autocomplete = new google.maps.places.Autocomplete(resourceAddress, { types: [ 'geocode' ] });
+      console.log(autocomplete)
+      google.maps.event.addDomListener(resourceAddress, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
