b593f5665f3b36ec50ad2898fcb6e654fd2e03f2
Liliane Momeni <liliane.momeni@btinternet.com>
2018-02-20 23:58:14 +0000

    did bookmarks

--- app/controllers/saved_events_controller.rb
+++ app/controllers/saved_events_controller.rb
@@ -1,19 +1,23 @@
 class SavedEventsController < ApplicationController
   def create
     saved_event = SavedEvent.new
+    #authorize saved_event
     @event = Event.find(params[:event_id])
     saved_event.event = @event
     @user = current_user
     saved_event.user = @user
     saved_event.save
-    redirect_to event_path(@event)
+    #redirect_to event_path(@event)
   end
 
   def destroy
     saved_event = SavedEvent.find(params[:id])
+    @user = current_user
+    #authorize saved_event
     @event = saved_event.event
     saved_event&.destroy
-    redirect_to event_path(@event)
+    #redirect_to event_path(@event)
+    render :create
   end
 
 end
--- app/controllers/saved_resources_controller.rb
+++ app/controllers/saved_resources_controller.rb
@@ -1,22 +1,25 @@
 class SavedResourcesController < ApplicationController
   def create
     saved_resource = SavedResource.new
+    #authorize saved_resource
     @resource = Resource.find(params[:resource_id])
     saved_resource.resource = @resource
-    saved_resource.user = current_user
+    @user = current_user
+    saved_resource.user = @user
     saved_resource.save
 
-    redirect_to resource_path(@resource)
+    #redirect_to resource_path(@resource)
   end
 
   def destroy
     saved_resource = SavedResource.find(params[:id])
-    # @user = current_user
+    #authorize saved_resource
+    @user = current_user
     @resource = saved_resource.resource
     saved_resource&.destroy
 
-    # render :create
-    redirect_to resource_path(@resource)
+    render :create
+    #redirect_to resource_path(@resource)
   end
 
 end
