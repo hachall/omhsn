8299d52b32a3af383878cd92a4c85d8304869aaf
Hamish Hall <hachall@hotmail.com>
2018-02-21 15:36:28 +0000

    sanitizing for devise - to save users to relogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
   end
-  
+
   include Pundit
 
   # Pundit: white-list approach.
