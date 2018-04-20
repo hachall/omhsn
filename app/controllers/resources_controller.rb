34663cdf327d4b818539e16f46f49cc180974a6d
Hamish Hall <hachall@hotmail.com>
2018-04-20 19:53:34 +0100

    example

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -7,6 +7,7 @@ class ResourcesController < ApplicationController
       @resources = policy_scope(Resource).order(name: :asc)
     elsif params[:query]
       @resources = policy_scope(Resource).search(params[:query])
+      # example
     else
       @resources = policy_scope(Resource).order(name: :asc)
     end
